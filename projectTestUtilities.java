3f94936565fe94d70036371a830efe2983f30912
boemaddie <64502325+boemaddie@users.noreply.github.com>
2020-05-01 15:20:26 -0700

    Update projectTestUtilities.java

--- projectTestUtilities.java
+++ projectTestUtilities.java
@@ -30,43 +30,51 @@ public class projectTestUtilities
 			choice = getChoice();
             switch (choice) 
             {
-			case 1: {
+			case 0: {
 				openDB();
 				break;
 			}
+			case 1: {
+				callAddCourse();
+				break;
+			}
 			case 2: {
-				addCourse();
+				callDeleteStudent();
 				break;
 			}
 			case 3: {
-				deleteStudent();
+				callReplace300Elective();
 				break;
 			}
 			case 4: {
-			    replace300Elective();
+				callAvailableCourses();
 				break;
 			}
 			case 5: {
-				availableCourses();
+				callGetAdvisees();
 				break;
 			}
 			case 6: {
-				getAdvisees();
+				callEditReport();
 				break;
 			}
 			case 7: {
-				editReport();
+				callViewCourses();
 				break;
 			}
 			case 8: {
-				viewCourses();
+				callViewGP();
 				break;
 			}
 			case 9: {
+				callGenerateGP();
+				break;
+			}
+			case 19: {
 				testObj.closeDB(); // Close the DB connection 
 				break;
 			}
-			case 10: {
+			case 20: {
 				done = true;
 				System.out.println("Good bye");
 				break;
@@ -79,7 +87,7 @@ public class projectTestUtilities
     // Display Menu! 
     static void displaymenu() 
     {
-		System.out.println("1)  Call openDB(String, String)");
+		System.out.println("0)  Call openDB(String, String)");
 		System.out.println("2)  Call addCourse(String, String, String, String, String, String, String)");
 		System.out.println("3)  Call deleteStudent(String)");
 		System.out.println("4)  Call replace300Elective()");
@@ -87,8 +95,8 @@ public class projectTestUtilities
 		System.out.println("6)  Call getAdvisees(String)");
 		System.out.println("7)  Call editReport(String, String, String)");
 		System.out.println("8)  Call viewCourses()");
-		System.out.println("9)  Close the DB");
-		System.out.println("10) Quit");
+		System.out.println("19)  Close the DB");
+		System.out.println("20) Quit");
     }
 
     // Retrieves which method the user would like to run. 
@@ -96,7 +104,7 @@ public class projectTestUtilities
     {
 		String input;
 		int i = 0;
-        while (i < 1 || i > 10) 
+        while (i < 0 || i > 21) 
         {
 			try {
 				System.out.print("Please enter an integer between 1-12: ");
@@ -126,7 +134,7 @@ public class projectTestUtilities
     
     // Test addCourse(String courseName, String department, String courseNumber, 
 	// String numCredits, String description, String semester, String year) Method 
-    static void addCourse() throws SQLException
+    static void callAddCourse() throws SQLException
     {
     	String courseName = "Cloud Computing";
     	String department = "CSCI";
@@ -140,37 +148,47 @@ public class projectTestUtilities
     }
 
     // Test deleteStudent(String) Method
-    static void deleteStudent() throws SQLException
+    static void callDeleteStudent() throws SQLException
     {
 
     }
 
     // Test replace300Elective() Method
-    static void replace300Elective() throws SQLException
+    static void callReplace300Elective() throws SQLException
     {
 
     }
 
     // Test availableCourses(String) Method
-    static void availableCourses() throws SQLException
+    static void callAvailableCourses() throws SQLException
     {
 
     }
 
     // Test getAdvisees(String) Method
-    static void getAdvisees() throws SQLException
+    static void callGetAdvisees() throws SQLException
     {
 
     }
 
     // Test editReport(String, String, String) Method
-    static void editReport() throws SQLException
+    static void callEditReport() throws SQLException
     {
 
     }
 
     // Test viewCourses() Method
-    static void viewCourses() throws SQLException
+    static void callViewCourses() throws SQLException
+    {
+
+    }
+    // Test viewGP() Method
+	static void callViewGP() throws SQLException
+    {
+
+    }
+    // Test generateGP() Method
+	static void callGenerateGP() throws SQLException
     {
 
     }
